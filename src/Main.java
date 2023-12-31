01fd68013c857476286294295eaf06b5ea2cd991
Edson Ramos <edson-ramos54@hotmail.com>
2023-12-31 13:55:47 -0300

    create class Position

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
+        Position position = new Position(3,5);
 
-        
+
+        System.out.println(position);
     }
 }
