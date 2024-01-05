81fa1673c9130442126a210a15f766028b9c9bf4
Edson Ramos <edson-ramos54@hotmail.com>
2024-01-05 18:17:42 -0300

    possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardGame.Board;
 import boardGame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
@@ -15,6 +15,4 @@ public class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
-
-
 }
