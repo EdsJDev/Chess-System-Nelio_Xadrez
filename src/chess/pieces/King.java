7c434d34970e3ae5f624830f8aa8a20cba13b669
Edson Ramos <edson-ramos54@hotmail.com>
2024-01-06 20:22:25 -0300

    possible moviments of the king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardGame.Board;
+import boardGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,34 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
+
     @Override
     public boolean[][] possibleMoves() {
-        boolean[][] mat = new boolean[getBoard().getRows()] [getBoard().getColumns()];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        int[][] directions = {
+                {-1, 0}, {1, 0},    // above, below
+                {0, -1}, {0, 1},    // left, right
+                {-1, -1}, {-1, 1},  // NW, NE
+                {1, -1}, {1, 1}     // SW, SE
+        };
+
+        for (int[] dir : directions) {
+            int newRow = position.getRow() + dir[0];
+            int newCol = position.getColumn() + dir[1];
+
+            Position p = new Position(newRow, newCol);
+
+            if (getBoard().positionExists(p) && canMove(p)) {
+                mat[newRow][newCol] = true;
+            }
+        }
+
         return mat;
     }
 }
