f672b1e92dfc78cb1244ad908a3671ba66ab1029
Edson Ramos <edson-ramos54@hotmail.com>
2024-01-09 20:59:31 -0300

    Queen Logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -187,6 +187,8 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
+
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -203,6 +205,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
